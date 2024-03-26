dc2c996f4d2a3459d14a97c68ce1e6b94a4e61e9
vamshi <svamshichow@gmail.com>
2024-03-26 16:04:45 +0530

    Educationapp

---
+++ src/main/java/com/vamshi/education/Model/Student.java
@@ -0,0 +1,49 @@
+package com.vamshi.education.Model;
+
+public class Student {
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String PhoneNumber;
+    private String Message;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+}
